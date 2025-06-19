951547e38e345ae7e2e145ae09b51b34651e6976
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2025-06-19 11:22:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# api-alura-restfull
+ curso
---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const rotas = {
+    "/": "Curso de Node.js",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+
+};
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end(rotas[req.url]);
+});
+
+server.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}`);
+});
