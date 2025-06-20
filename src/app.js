c14a634572807cebc868cebd24811cea9b374d80
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2025-06-20 14:18:26 -0300

    function busca id

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ const livros = [
     }
 ];
 
+function buscaLivros(id){
+    return livros.findIndex(livro=>{
+        return livro.id === Number(id);
+    })
+}
+
 app.get("/", (req,res)=>{
     res.status(200).send("Curso de node.js");
 });
@@ -22,9 +28,15 @@ app.get("/livros", (req, res)=>{
     res.status(200).json(livros);
 } );
 
+app.get("/livros/:id", (req, res)=>{
+    const index = buscaLivros(req.params.id);
+    res.status(200).json(livros[index]);
+})
+
 app.post("/livros", (req,res)=>{
     livros.push(req.body);
     res.status(201).send("Livros cadastrado com sucesso!")
-})
+});
+
 
 export default app;
