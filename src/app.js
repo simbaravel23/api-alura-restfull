19ca2985992de7ae03c9a9e8a40c07e8f88826b1
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2025-06-20 13:40:27 -0300

    post

--- src/app.js
+++ src/app.js
@@ -2,9 +2,27 @@ import express from "express";
 
 const app = express();
 
+const livros = [
+    {
+        id: 1,
+        título: "O Senhor dos Anéis"
+    },
+    {
+        id: 2,
+        título: "O Hobbit"
+    }
+];
 
 app.get("/", (req,res)=>{
     res.status(200).send("Curso de node.js");
 });
+
+app.get("/livros", (req, res)=>{
+    res.status(200).json(livros);
+} );
+
+app.post("/livros", (req,res)=>{
+    livros.push(req.body);
+})
 
 export default app;
