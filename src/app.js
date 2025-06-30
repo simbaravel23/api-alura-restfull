fbcd5c11db4e1af6028b710a02b17004a9110432
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2025-06-30 15:13:53 -0300

    routs, controller

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbConnect.js";
-import livro from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 const conexao = await conectaNaDatabase();
 
@@ -12,25 +12,8 @@ conexao.once("open", ()=>{
     console.log("Conexão com o banco feita com sucesso")
 })
 
-const app = express();
-app.use(express.json());
-
-
-
-
-app.get("/", (req,res)=>{
-    res.status(200).send("Curso de node.js");
-});
-
-app.get("/livros", async (req, res)=>{
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
-} );
-
-app.get("/livros/:id", (req, res)=>{
-    const index = buscaLivros(req.params.id);
-    res.status(200).json(livros[index]);
-})
+const app = express ();
+routes(app);
 
 app.post("/livros", (req,res)=>{
     livros.push(req.body);
---
+++ src/controller/livroController.js
@@ -0,0 +1,21 @@
+import livro from "../models/Livro.js";
+
+class LivroController {
+
+
+    static async listarLivros (req,res){
+        const index = buscaLivros(req.params.id);
+        res.status(200).json(livros[index]);
+    }
+};
+
+    static async cadastrarLivros (req,res){
+        try {
+        const novoLivro = await livro.create(req.body);    
+        res.status(201).json({message:"criado com sucesso", livro: novoLivro})
+        }catch(erro){
+            res.status(500).json({message:`${erro.message}- falha ao cadastrar livro`});
+        }
+    };
+
+export default LivroController;
